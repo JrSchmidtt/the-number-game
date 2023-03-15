fea4181dce5bbcd6c81bac43d68ea1a1083e1415
JrSchmidtt <junior.schmidt.99@gmail.com>
2023-03-15 20:44:20 -0300

    modified:   game.cpp 	new file:   output/game

--- game.cpp
+++ game.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
-using namespace std; // using namespace std is a preprocessor directive that tells the compiler to use the standard namespace.
-// #include is a preprocessor directive that tells the compiler to include the iostream header file.
+using namespace std;
 
-// int main() is the entry point of the program and is the first function that is called when the program is executed.
 int main (){
-    cout << "Hello World!"; // cout is an object of the ostream class and << is the insertion operator.
+    cout << "Hello World!\n";
+    int secretNumber = 7;
+    cout << "The secret number is: " << secretNumber;
 }
